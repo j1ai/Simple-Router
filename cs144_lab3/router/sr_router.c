4f834e17e1b5c18262d2d337ba5ece75529c402b
Emilio Kartono <emiliokartono@gmail.com>
2019-09-29 16:45:33 -0400

    wip

--- cs144_lab3/router/sr_router.c
+++ cs144_lab3/router/sr_router.c
@@ -182,6 +182,8 @@ void sr_handle_icmp_ip_packet(struct sr_instance *sr, uint8_t *packet, unsigned 
 
     /* Set up the IP header */
     sr_ip_hdr_t *new_packet_ip_headers = (sr_ip_hdr_t *) (new_packet + sizeof(sr_ethernet_hdr_t));
+    new_packet_ip_headers->ip_v = ip_header->ip_v;
+    new_packet_ip_headers->ip_hl = ip_header->ip_hl;
     new_packet_ip_headers->ip_tos = ip_header->ip_tos;  /* ?? */
     new_packet_ip_headers->ip_len = ip_header->ip_len;  /* ?? */
     new_packet_ip_headers->ip_id = ip_header->ip_id;   /* ?? */
