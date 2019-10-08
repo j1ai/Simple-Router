ffdfef95f12786f8a067c9b51d170da782f4f5f3
Emilio Kartono <emiliokartono@gmail.com>
2019-10-08 14:19:11 -0400

    wip

--- cs144_lab3/router/sr_router.c
+++ cs144_lab3/router/sr_router.c
@@ -544,6 +544,7 @@ void sr_handle_foreign_ip_packet(struct sr_instance *sr, uint8_t *packet, unsign
  * Returns 1 if it is the packet for the router; else return 0
  */
 int is_ip_packet_for_me(struct sr_instance *sr, uint32_t ip_dest){
+    print_addr_ip_int(ip_dest);
     struct sr_if *temp_if_list = sr->if_list;
     while(temp_if_list){
       print_addr_ip_int(temp_if_list->ip);
