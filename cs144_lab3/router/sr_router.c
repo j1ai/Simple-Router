3f2bbbad72118aeb0207f87007f76feacda8f61c
Emilio Kartono <emiliokartono@gmail.com>
2019-10-10 00:07:56 -0400

    wip

--- cs144_lab3/router/sr_router.c
+++ cs144_lab3/router/sr_router.c
@@ -420,27 +420,6 @@ void sr_handle_port_unreachable_ip_packet(struct sr_instance *sr, uint8_t *packe
   sr_ip_hdr_t *ip_header = (sr_ip_hdr_t *)(packet + sizeof(sr_ethernet_hdr_t));
   sr_icmp_t3_hdr_t *icmp_header = (sr_icmp_t3_hdr_t *)(packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t));
 
-  int new_packet_len = sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t) + sizeof(sr_icmp_t3_hdr_t);
-  uint8_t *new_packet = malloc(new_packet_len);
-
-  /** Get the headers for the packet */
-  sr_ethernet_hdr_t *new_ethernet_header = (sr_ethernet_hdr_t *) new_packet;
-  sr_ip_hdr_t *new_ip_header = (sr_ip_hdr_t *)(new_packet + sizeof(sr_ethernet_hdr_t));
-  sr_icmp_t3_hdr_t *new_icmp_header = (sr_icmp_t3_hdr_t *)(new_packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t));
-
-  /** Set up the headers */
-  sr_setup_new_ethernet_headers(new_ethernet_header, ethernet_header->ether_dhost, ethernet_header->ether_shost, ethernet_header->ether_type);
-  sr_setup_new_ip_headers(new_ip_header, sizeof(sr_ip_hdr_t) + sizeof(sr_icmp_t3_hdr_t), ip_protocol_icmp, ip_header->ip_dst, ip_header->ip_src);
-  sr_setup_new_icmp3_headers(new_icmp_header, ip_header, 3, 3);
-
-  /** Send the packet */
-  printf("Sending ICMP Port Unreachable IP Packet!\n");
-  if (sr_send_packet(sr, new_packet, new_packet_len, interface) != 0) {
-    fprintf(stderr, "ERROR: Packet sent unsuccessfully\n");
-  }
-  free(new_packet);
-
-  return;
 
   /* Swap the source and destination MAC addresses */
   uint8_t new_ether_dhost[6];
