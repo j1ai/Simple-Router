f77989e241e3a48143faf9500add84687be43e2c
Emilio Kartono <emiliokartono@gmail.com>
2019-10-09 23:43:59 -0400

    wip

--- cs144_lab3/router/sr_router.c
+++ cs144_lab3/router/sr_router.c
@@ -397,7 +397,7 @@ void sr_handle_net_unreachable_ip_packet(struct sr_instance *sr, uint8_t *packet
   sr_setup_new_icmp3_headers(new_icmp_header, ip_header);
 
   /** Send the packet */
-  sr_send_packet(sr, new_packet, new_packet_len, out_iface->name);
+  sr_send_packet(sr, new_packet, new_packet_len, interface);
   free(new_packet);
   printf("Sent ICMP Net Unreachable Reply Packet!\n");
 }
