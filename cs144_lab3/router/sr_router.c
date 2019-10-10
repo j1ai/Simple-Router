d85d37971dfc5fd2382716dbd42b65e1fee846ae
Emilio Kartono <emiliokartono@gmail.com>
2019-10-10 18:59:43 -0400

    wip

--- cs144_lab3/router/sr_router.c
+++ cs144_lab3/router/sr_router.c
@@ -406,7 +406,6 @@ void sr_handle_net_unreachable_ip_packet(struct sr_instance *sr, uint8_t *packet
 void sr_handle_port_unreachable_ip_packet(struct sr_instance *sr, uint8_t *packet, unsigned int len, char *interface)
 {
   printf("Port Unreachable IP Packet!\n");
-  struct sr_if *sr_if_interface = sr_get_interface(sr, interface);
 
   /* Get the ethernet header */
   sr_ethernet_hdr_t *ethernet_header = (sr_ethernet_hdr_t *) packet;
@@ -422,7 +421,7 @@ void sr_handle_port_unreachable_ip_packet(struct sr_instance *sr, uint8_t *packe
   sr_icmp_t3_hdr_t *new_icmp_header = (sr_icmp_t3_hdr_t *)(new_packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t));
 
   /** Set up the headers */
-  sr_setup_new_ethernet_headers(new_ethernet_header, sr_if_interface->addr, ethernet_header->ether_shost, ethernet_header->ether_type);
+  sr_setup_new_ethernet_headers(new_ethernet_header, ethernet_header->ether_dhost, ethernet_header->ether_shost, ethernet_header->ether_type);
   sr_setup_new_ip_headers(new_ip_header, sizeof(sr_ip_hdr_t) + sizeof(sr_icmp_t3_hdr_t), ip_protocol_icmp, ip_header->ip_dst, ip_header->ip_src);
   sr_setup_new_icmp3_headers(new_icmp_header, ip_header, 3, 3);
 
@@ -495,7 +494,6 @@ void sr_handle_host_unreachable_ip_packet(struct sr_instance *sr, uint8_t *packe
 void sr_handle_time_exceeded_ip_packet(struct sr_instance *sr, uint8_t *packet, unsigned int len, char *interface)
 {
   printf("Received TTL execeeded IP Packet!\n");
-  struct sr_if *sr_if_interface = sr_get_interface(sr, interface);
 
   sr_ethernet_hdr_t *ethernet_header = (sr_ethernet_hdr_t *) packet;
   sr_ip_hdr_t *ip_header = (sr_ip_hdr_t *) (packet + sizeof(sr_ethernet_hdr_t));
@@ -509,7 +507,7 @@ void sr_handle_time_exceeded_ip_packet(struct sr_instance *sr, uint8_t *packet, 
   sr_ip_hdr_t *new_ip_header = (sr_ip_hdr_t *) (new_packet + sizeof(sr_ethernet_hdr_t));
   sr_icmp_t3_hdr_t *new_icmp_header = (sr_icmp_t3_hdr_t *) (new_packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t));
 
-  sr_setup_new_ethernet_headers(new_ethernet_header, sr_if_interface->addr, ethernet_header->ether_shost, ethernet_header->ether_type);
+  sr_setup_new_ethernet_headers(new_ethernet_header, ethernet_header->ether_dhost, ethernet_header->ether_shost, ethernet_header->ether_type);
   sr_setup_new_ip_headers(new_ip_header, sizeof(sr_ip_hdr_t) + sizeof(sr_icmp_t3_hdr_t), ip_protocol_icmp, ip_header->ip_dst, ip_header->ip_src);
   sr_setup_new_icmp3_headers(new_icmp_header, ip_header, 11, 0);
 
