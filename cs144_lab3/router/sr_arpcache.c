2594c9c238b64b4490f1ba29d9fa1965f71c7f41
Emilio Kartono <emiliokartono@gmail.com>
2019-10-10 14:02:55 -0400

    wip

--- cs144_lab3/router/sr_arpcache.c
+++ cs144_lab3/router/sr_arpcache.c
@@ -97,6 +97,8 @@ void handle_arpreq(struct sr_instance *sr, struct sr_arpreq *request) {
     time_t cur_time;
     time (&cur_time);
 
+    printf("I AM HERE\n");
+
     /** Check if the time sent before is greater than 1 second */
     if (difftime(cur_time, request->sent) > 1.0) {
 
