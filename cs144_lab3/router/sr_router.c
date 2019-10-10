b7bc9ca7ddd6b99a8e8fd27308259e8e9ee8ad5e
Emilio Kartono <emiliokartono@gmail.com>
2019-10-10 14:42:09 -0400

    wip

--- cs144_lab3/router/sr_router.c
+++ cs144_lab3/router/sr_router.c
@@ -579,8 +579,7 @@ void sr_handle_foreign_ip_packet(struct sr_instance *sr, uint8_t *packet, unsign
 	    print_hdrs(arp_packet, arp_packet_len);
 
 	    /* Send ARP request */
-	    struct sr_arpreq *arp_req = sr_arpcache_queuereq(&(sr->cache), ip_header->ip_dst,
-                                                              arp_packet, arp_packet_len, interface);
+	    struct sr_arpreq *arp_req = sr_arpcache_queuereq(&(sr->cache), ip_header->ip_dst, /*arp_packet*/ packet, /*arp_packet_len*/len, interface);
       time_t cur_time;
       time (&cur_time);
       arp_req->sent = cur_time;
