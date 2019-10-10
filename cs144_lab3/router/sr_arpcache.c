54c87778f3f44e49865316f337d1fd7b4595658a
Emilio Kartono <emiliokartono@gmail.com>
2019-10-10 14:04:39 -0400

    wip

--- cs144_lab3/router/sr_arpcache.c
+++ cs144_lab3/router/sr_arpcache.c
@@ -97,11 +97,10 @@ void handle_arpreq(struct sr_instance *sr, struct sr_arpreq *request) {
     time_t cur_time;
     time (&cur_time);
 
+    printf("I AM HERE\n");
+
     /** Check if the time sent before is greater than 1 second */
     if (difftime(cur_time, request->sent) > 1.0) {
-
-        printf("I AM HERE\n");
-
         /** Check if the number of times sent is greater than 5*/
         if (request->times_sent >= 5) {
 	    printf("ARP times_sent >= 5!\n");
