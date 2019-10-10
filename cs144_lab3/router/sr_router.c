724ed92a5403107f7e81b6695fe5c0ddc57e50c5
Emilio Kartono <emiliokartono@gmail.com>
2019-10-10 16:39:16 -0400

    wip

--- cs144_lab3/router/sr_router.c
+++ cs144_lab3/router/sr_router.c
@@ -579,8 +579,9 @@ void sr_handle_foreign_ip_packet(struct sr_instance *sr, uint8_t *packet, unsign
 	    /* Add fields to ethernet packet */
 	    struct sr_if *src_interface = sr_get_interface(sr, interface);
 	    sr_ethernet_hdr_t *arp_packet_eth_headers = (sr_ethernet_hdr_t *) arp_packet;
-      uint8_t broadcast_mac_addr_2[6] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
-	    memcpy(arp_packet_eth_headers->ether_dhost, broadcast_mac_addr_2, sizeof(uint8_t) * ETHER_ADDR_LEN);
+      for (int i = 0; i < ETHER_ADDR_LEN; i++) {
+        arp_packet_eth_headers->ether_dhost[i] = 255;          
+      }
 	    memcpy(arp_packet_eth_headers->ether_shost, outgoing_interface->addr, sizeof(uint8_t) * ETHER_ADDR_LEN);
 	    arp_packet_eth_headers->ether_type = htons(ethertype_arp);
  
@@ -594,9 +595,9 @@ void sr_handle_foreign_ip_packet(struct sr_instance *sr, uint8_t *packet, unsign
 
 	    memcpy(arp_packet_arp_headers->ar_sha, source_interface->addr/*src_interface->addr*/, sizeof(uint8_t) * ETHER_ADDR_LEN);
 	    arp_packet_arp_headers->ar_sip = ip_header->ip_src;/*src_interface->ip;*/
-	
-      uint8_t broadcast_mac_addr[6] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-	    memcpy(arp_packet_arp_headers->ar_tha, broadcast_mac_addr, sizeof(uint8_t) * ETHER_ADDR_LEN);
+      for (int i = 0; i < ETHER_ADDR_LEN; i++) {
+        arp_packet_arp_headers->ar_tha[i] = 255;
+      }
 	    arp_packet_arp_headers->ar_tip = ip_header->ip_dst;	    
 
 	    printf("ARP Packet below!! \n");
