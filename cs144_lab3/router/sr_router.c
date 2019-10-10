f30494fc33b1f302509ecb25ec2b18aa57534168
Emilio Kartono <emiliokartono@gmail.com>
2019-10-10 18:51:04 -0400

    wip

--- cs144_lab3/router/sr_router.c
+++ cs144_lab3/router/sr_router.c
@@ -493,7 +493,6 @@ void sr_handle_host_unreachable_ip_packet(struct sr_instance *sr, uint8_t *packe
  *---------------------------------------------------------------------*/
 void sr_handle_time_exceeded_ip_packet(struct sr_instance *sr, uint8_t *packet, unsigned int len, char *interface)
 {
-  /* TODO: Do something if the IP packet TTL is 0 */
   printf("Received TTL execeeded IP Packet!\n");
 
   sr_ethernet_hdr_t *ethernet_header = (sr_ethernet_hdr_t *) packet;
@@ -511,6 +510,8 @@ void sr_handle_time_exceeded_ip_packet(struct sr_instance *sr, uint8_t *packet, 
   sr_setup_new_ethernet_headers(new_ethernet_header, ethernet_header->ether_dhost, ethernet_header->ether_shost, ethernet_header->ether_type);
   sr_setup_new_ip_headers(new_ip_header, sizeof(sr_ip_hdr_t) + sizeof(sr_icmp_t3_hdr_t), ip_protocol_icmp, ip_header->ip_dst, ip_header->ip_src);
   sr_setup_new_icmp3_headers(new_icmp_header, ip_header, 11, 0);
+
+  new_ip_header->ip_ttl = 0;
 
   printf("Sending TTL exceeded IP Packet\n");
 
