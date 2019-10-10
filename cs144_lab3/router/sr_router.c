1b19024fed66c30df35d0c5f0cc6b68a364b5376
j1ai <laijustinholam@gmail.com>
2019-10-10 03:32:45 -0400

    test

--- cs144_lab3/router/sr_router.c
+++ cs144_lab3/router/sr_router.c
@@ -459,7 +459,7 @@ void sr_handle_host_unreachable_ip_packet(struct sr_instance *sr, uint8_t *packe
   struct sr_if *packet_if = sr_get_interface(sr,interface);
 
   /** Set up the headers */
-  sr_setup_new_ethernet_headers(new_ethernet_header, ethernet_header->ether_dhost, ethernet_header->ether_shost, ethernet_header->ether_type);
+  sr_setup_new_ethernet_headers(new_ethernet_header, ethernet_header->ether_shost, ethernet_header->ether_dhost, ethernet_header->ether_type);
   sr_setup_new_ip_headers(new_ip_header, sizeof(sr_ip_hdr_t) + sizeof(sr_icmp_t3_hdr_t), ip_protocol_icmp, ip_header->ip_src, packet_if->ip);
   sr_setup_new_icmp3_headers(new_icmp_header, ip_header, 3, 1);
 
